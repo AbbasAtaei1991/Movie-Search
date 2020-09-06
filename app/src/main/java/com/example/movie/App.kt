3c4e536e4ff77812cfd1255001b94c5a2242c52c
Abbas <a.ataei1991@gmail.com>
2020-09-06 12:27:01 +0430

    add placeholder and error image to coil

--- app/src/main/java/com/example/movie/App.kt
+++ app/src/main/java/com/example/movie/App.kt
@@ -1,14 +1,13 @@
 package com.example.movie
 
 import android.app.Application
-import androidx.multidex.MultiDexApplication
 import com.example.movie.di.module.appModule
 import com.example.movie.di.module.repoModule
 import com.example.movie.di.module.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : MultiDexApplication() {
+class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/example/movie/utils/BindingAdapter.kt
+++ app/src/main/java/com/example/movie/utils/BindingAdapter.kt
@@ -4,15 +4,13 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
-import com.bumptech.glide.Glide
+import com.example.movie.R
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, url: String?) {
     imageView.load("https://image.tmdb.org/t/p/w500$url") {
         transformations(RoundedCornersTransformation(8f))
+        placeholder(R.drawable.filmstrip)
+        error(R.drawable.movie_roll)
     }
-//    Glide.with(imageView)
-//        .load("https://image.tmdb.org/t/p/w500$url")
-//        .centerCrop()
-//        .into(imageView)
 }
