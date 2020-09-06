20969aa0f696871e4a17f578f32db109a4e788e9
Abbas <a.ataei1991@gmail.com>
2020-09-06 12:04:19 +0430

    replace glide with coil

--- app/src/main/java/com/example/movie/App.kt
+++ app/src/main/java/com/example/movie/App.kt
@@ -1,13 +1,14 @@
 package com.example.movie
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.example.movie.di.module.appModule
 import com.example.movie.di.module.repoModule
 import com.example.movie.di.module.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : Application() {
+class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/example/movie/utils/BindingAdapter.kt
+++ app/src/main/java/com/example/movie/utils/BindingAdapter.kt
@@ -2,12 +2,17 @@ package com.example.movie.utils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import coil.load
+import coil.transform.RoundedCornersTransformation
 import com.bumptech.glide.Glide
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, url: String?) {
-    Glide.with(imageView)
-        .load("https://image.tmdb.org/t/p/w500$url")
-        .centerCrop()
-        .into(imageView)
+    imageView.load("https://image.tmdb.org/t/p/w500$url") {
+        transformations(RoundedCornersTransformation(8f))
+    }
+//    Glide.with(imageView)
+//        .load("https://image.tmdb.org/t/p/w500$url")
+//        .centerCrop()
+//        .into(imageView)
 }
